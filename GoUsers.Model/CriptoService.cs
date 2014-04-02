15425089572c1dc026546e98193b0c1e5968b3c3
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2014-04-02 13:14:40 -0300

    Testes senha criptografada

---
+++ GoUsers.Model/CriptoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GoUsers.Model
+{
+  public class CriptoService
+  {
+    public string GetHash(string senha)
+    {
+      var sha1 = new SHA1CryptoServiceProvider();
+      byte[] hashData = sha1.ComputeHash(Encoding.Default.GetBytes(senha)); 
+      return Convert.ToBase64String(hashData);
+    }
+  }
+}
---
+++ GoUsersTests/PasswordTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoUsers.Model;
+
+namespace GoUsers.Tests
+{
+  [TestClass]
+  public class PasswordTests
+  {
+    [TestMethod]
+    public void SenhaDeveEstarCodificadaComSHA1Base64()
+    {
+      var senha = "badger";
+      var senhaCriptografada = "ThmbShxAtJepX80c2JY1FzOEmUk=";
+      var criptoService = new CriptoService();
+      Assert.AreEqual(senhaCriptografada, criptoService.GetHash(senha));
+    }
+  }
+}
