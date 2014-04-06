72bdf07fb14e3c0b71a6e868ccd896943d4a4d33
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2014-04-06 14:59:53 -0300

    Testes

--- GoUsersTests/GerenciadorUsuariosTests.cs
+++ GoUsersTests/GerenciadorUsuariosTests.cs
@@ -69,6 +69,7 @@ namespace GoUsers.Tests
     [TestMethod]
     public void UsuariosExcluidosDevemSerExcluidosDoArquivo()
     {
+      gerenciadorUsuarios.AdicionarUsuario(usuario);
       gerenciadorUsuarios.ExcluirUsuario(usuario);
       gerenciadorUsuarios.Salvar();
       gerenciadorUsuarios = new GerenciadorUsuarios(arquivoTeste);
