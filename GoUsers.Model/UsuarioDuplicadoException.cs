ef1e8ad9488418ace75143fda38e2d5965b97a3a
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2014-04-06 15:22:51 -0300

    Testes

---
+++ GoUsers.Model/UsuarioDuplicadoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoUsers.Model
+{
+  public class UsuarioDuplicadoException : Exception
+  {
+  }
+}
